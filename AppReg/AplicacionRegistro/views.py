e2d84f6d19b9b4cf479c8b4a8f95e0599aeac5a4
Manuel Lujan <mleolujang@gmail.com>
2023-09-08 11:07:39 -0300

    primera partes

---
+++ AppReg/AplicacionRegistro/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import HttpResponse
+
+def inicio(request):
+    return render(request,"AplicacionRegistro/inicio.html")
+
+
+def registro(request):
+    pass
+
+
+# Create your views here.
